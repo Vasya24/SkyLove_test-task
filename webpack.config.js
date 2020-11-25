76a72e14ac2cab7274066e7d8748f9da45d50d35
Vasiliy Smirnov <vasya.smirnoff24@yandex.ru>
2020-11-25 21:14:47 +0300

    Almost done with layout

--- webpack.config.js
+++ webpack.config.js
@@ -20,13 +20,13 @@ module.exports = {
         },
       },
       {
-        test: /\.(woff(2)?|ttf|eot|otf\svg)(\?v=\d+\.\d+\.\d+)?$/,
+        test: /\.(woff(2)?|ttf|eot|otf|svg)(\?v=\d+\.\d+\.\d+)?$/,
         use: [
           {
             loader: 'file-loader',
             options: {
               name: '[name].[ext]',
-              outputPath: 'fonts/',
+              outputPath: 'Fonts/',
             },
           },
         ],
