ea1aded028cfb3d4daa32650d85570eb8183e532
Vasiliy Smirnov <vasya.smirnoff24@yandex.ru>
2020-11-30 13:36:33 +0300

    Still not finished but pretty well

--- src/js/app.js
+++ src/js/app.js
@@ -1,36 +1,18 @@
-const $ = require('jquery')
-
-let burger = document.querySelector('.burger');
-let dropMain = document.querySelector('.dropdown-left');
-
-// burger.addEventListener('mouseover', () => {
-//   dropMain.style.display = 'block'
-// })
-// dropMain.addEventListener('mouseout', () => {
-//   dropMain.style.display = 'none'
-// })
-
-// burger.addEventListener('mouseover', () => {
-//   dropMain.classList.add('active')
-// });
-// dropMain.addEventListener('mouseout', () => {
-//   dropMain.classList.remove('active')
-// })
+const $ = require('jquery');
 
 $('.burger').mouseover(() => {
-  $('.dropdown-left').addClass('active')
-})
+  $('.dropdown-left').addClass('active');
+});
 $('.dropdown-left').mouseout(() => {
-  $('.dropdown-left').removeClass('active')
-})
+  $('.dropdown-left').removeClass('active');
+});
 
-
-$('.mark-box').click(function(event) {
+$('.mark-box').click(function () {
   $('.selected').removeClass('selected');
   $(this).addClass('selected');
 });
 
-$('.posts-menu-item').click(function() {
+$('.posts-menu-item').click(function () {
   $('.active').removeClass('active');
-  $(this).addClass(('active'))
-})
+  $(this).addClass(('active'));
+});
