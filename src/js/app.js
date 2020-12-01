77f28f929e976144f293dee1afd5f49d4d0d5e65
Vasiliy Smirnov <vasya.smirnoff24@yandex.ru>
2020-12-01 15:43:40 +0300

    Done with the third dropdown and fixed some bugs regarding mouse events

--- src/js/app.js
+++ src/js/app.js
@@ -3,9 +3,21 @@ const $ = require('jquery');
 $('.burger').mouseover(() => {
   $('.dropdown-left').addClass('active');
 });
-$('.dropdown-left').mouseout(() => {
+$('.dropdown-left').mouseleave(() => {
   $('.dropdown-left').removeClass('active');
 });
+$('.address-choose').mouseover(() => {
+  $('.addresses-dropdown').addClass('active');
+})
+$('.addresses-dropdown').mouseleave(() => {
+  $('.addresses-dropdown').removeClass('active');
+})
+$('.current-user').mouseover(() => {
+  $('.dropdown-user').addClass('active')
+})
+$('.dropdown-user').mouseleave(() => {
+  $('.dropdown-user').removeClass('active')
+})
 
 $('.mark-box').click(function () {
   $('.selected').removeClass('selected');
@@ -16,3 +28,18 @@ $('.posts-menu-item').click(function () {
   $('.active').removeClass('active');
   $(this).addClass(('active'));
 });
+
+$('.ah-item').click(function () {
+  $('.active').removeClass('active');
+  $(this).addClass('active');
+});
+
+
+$('.addressbox').mouseover(function() {
+  $('.hovered').removeClass('hovered')
+  $(this).addClass('hovered')
+  if ($('.hovered')) {
+    $(this).children('.hovered').removeClass('hovered')
+    $(this).children('img').addClass('hovered')
+  }
+})
