8d9969e1f67f28932854c1c6657e7a1735130914
Vasiliy Smirnov <vasya.smirnoff24@yandex.ru>
2020-12-03 11:56:43 +0300

    DONE

--- src/js/app.js
+++ src/js/app.js
@@ -11,46 +11,46 @@ $('.dropdown-left').mouseleave(() => {
 // Выпадашка адресов
 $('.address-choose').mouseover(() => {
   $('.addresses-dropdown').addClass('active');
-})
+});
 $('.addresses-dropdown').mouseleave(() => {
   $('.addresses-dropdown').removeClass('active');
-})
+});
 // Внутри адресной выпадашки инициировал подкрашивание звездочек при наведении на каждый бокс
-$('.addressbox').mouseover(function() {
-  $('.hovered').removeClass('hovered')
-  $(this).addClass('hovered')
+$('.addressbox').mouseover(function () {
+  $('.hovered').removeClass('hovered');
+  $(this).addClass('hovered');
   if ($('.hovered')) {
-    $(this).children('.hovered').removeClass('hovered')
-    $(this).children('img').addClass('hovered')
+    $(this).children('.hovered').removeClass('hovered');
+    $(this).children('img').addClass('hovered');
   }
-})
+});
 
 // Выпадашка профиля
 $('.current-user').mouseover(() => {
-  $('.dropdown-user').addClass('active')
-})
+  $('.dropdown-user').addClass('active');
+});
 $('.dropdown-user').mouseleave(() => {
-  $('.dropdown-user').removeClass('active')
-})
+  $('.dropdown-user').removeClass('active');
+});
 
 // Выпадашка "За темой следят"
 $('#drop-followers').mouseover(() => {
-  $('.followers-dropdown').addClass('active')
-})
+  $('.followers-dropdown').addClass('active');
+});
 $('.followers-dropdown').mouseleave(() => {
-  $('.followers-dropdown').removeClass('active')
-})
+  $('.followers-dropdown').removeClass('active');
+});
 
 // Выпадашка лайков
 let likes = 15;
-$('.dd-likes').html(likes)
+$('.dd-likes').html(likes);
 $('#drop-likes').click(() => {
   $('.dd-likes').html(++likes);
-  $('.likes-dropdown').addClass('active')
-})
+  $('.likes-dropdown').addClass('active');
+});
 $('.likes-dropdown').mouseleave(() => {
-  $('.likes-dropdown').removeClass('active')
-})
+  $('.likes-dropdown').removeClass('active');
+});
 
 // Переключаемые вкладки во всех секциях
 $('.mark-box').click(function () {
