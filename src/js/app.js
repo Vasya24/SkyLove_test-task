1e41c62af933067c8cf70d51c6bf6d765d97d954
Vasiliy Smirnov <vasya.smirnoff24@yandex.ru>
2020-12-03 11:50:47 +0300

    DONE

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,31 @@
 const $ = require('jquery');
 
+// Выпадашка слева
 $('.burger').mouseover(() => {
   $('.dropdown-left').addClass('active');
 });
 $('.dropdown-left').mouseleave(() => {
   $('.dropdown-left').removeClass('active');
 });
+
+// Выпадашка адресов
 $('.address-choose').mouseover(() => {
   $('.addresses-dropdown').addClass('active');
 })
 $('.addresses-dropdown').mouseleave(() => {
   $('.addresses-dropdown').removeClass('active');
 })
+// Внутри адресной выпадашки инициировал подкрашивание звездочек при наведении на каждый бокс
+$('.addressbox').mouseover(function() {
+  $('.hovered').removeClass('hovered')
+  $(this).addClass('hovered')
+  if ($('.hovered')) {
+    $(this).children('.hovered').removeClass('hovered')
+    $(this).children('img').addClass('hovered')
+  }
+})
+
+// Выпадашка профиля
 $('.current-user').mouseover(() => {
   $('.dropdown-user').addClass('active')
 })
@@ -19,6 +33,26 @@ $('.dropdown-user').mouseleave(() => {
   $('.dropdown-user').removeClass('active')
 })
 
+// Выпадашка "За темой следят"
+$('#drop-followers').mouseover(() => {
+  $('.followers-dropdown').addClass('active')
+})
+$('.followers-dropdown').mouseleave(() => {
+  $('.followers-dropdown').removeClass('active')
+})
+
+// Выпадашка лайков
+let likes = 15;
+$('.dd-likes').html(likes)
+$('#drop-likes').click(() => {
+  $('.dd-likes').html(++likes);
+  $('.likes-dropdown').addClass('active')
+})
+$('.likes-dropdown').mouseleave(() => {
+  $('.likes-dropdown').removeClass('active')
+})
+
+// Переключаемые вкладки во всех секциях
 $('.mark-box').click(function () {
   $('.selected').removeClass('selected');
   $(this).addClass('selected');
@@ -33,13 +67,3 @@ $('.ah-item').click(function () {
   $('.active').removeClass('active');
   $(this).addClass('active');
 });
-
-
-$('.addressbox').mouseover(function() {
-  $('.hovered').removeClass('hovered')
-  $(this).addClass('hovered')
-  if ($('.hovered')) {
-    $(this).children('.hovered').removeClass('hovered')
-    $(this).children('img').addClass('hovered')
-  }
-})
